eb60471e2a2deaf666e61825ec5df3f20e427807
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-11-21 16:00:30 +0530

    add crew

--- src/insta/crew.py
+++ src/insta/crew.py
@@ -8,47 +8,85 @@ from crewai.project import CrewBase, agent, crew, task
 # from crewai_tools import SerperDevTool
 
 @CrewBase
-class Insta():
-	"""Insta crew"""
+class InstagramCrew:
+    """Instagram crew"""
 
-	agents_config = 'config/agents.yaml'
-	tasks_config = 'config/tasks.yaml'
+    agents_config = "config/agents.yaml"
+    tasks_config = "config/tasks.yaml"
 
-	@agent
-	def researcher(self) -> Agent:
-		return Agent(
-			config=self.agents_config['researcher'],
-			# tools=[MyCustomTool()], # Example of custom tool, loaded on the beginning of file
-			verbose=True
-		)
+    @agent
+    def market_researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config["market_researcher"],
+            tools=[
+              SearchTools.search_internet,
+              SearchTools.search_instagram,
+              SearchTools.open_page,
+            ],
+            verbose=True,
+        )
 
-	@agent
-	def reporting_analyst(self) -> Agent:
-		return Agent(
-			config=self.agents_config['reporting_analyst'],
-			verbose=True
-		)
+    @agent
+    def content_strategist(self) -> Agent:
+        return Agent(config=self.agents_config["content_strategist"], verbose=True)
 
-	@task
-	def research_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['research_task'],
-		)
+    @agent
+    def visual_creator(self) -> Agent:
+        return Agent(
+            config=self.agents_config["visual_creator"],
+            verbose=True,
+            allow_delegation=False,
+        )
 
-	@task
-	def reporting_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['reporting_task'],
-			output_file='report.md'
-		)
+    @agent
+    def copywriter(self) -> Agent:
+        return Agent(config=self.agents_config["copywriter"], verbose=True)
 
-	@crew
-	def crew(self) -> Crew:
-		"""Creates the Insta crew"""
-		return Crew(
-			agents=self.agents, # Automatically created by the @agent decorator
-			tasks=self.tasks, # Automatically created by the @task decorator
-			process=Process.sequential,
-			verbose=True,
-			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
-		)
+    @task
+    def market_research(self) -> Task:
+        return Task(
+            config=self.tasks_config["market_research"],
+            agent=self.market_researcher(),
+            output_file="market_research.md",
+        )
+
+    @task
+    def content_strategy_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["content_strategy"],
+            agent=self.content_strategist(),
+        )
+
+    @task
+    def visual_content_creation_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["visual_content_creation"],
+            agent=self.visual_creator(),
+            output_file="visual-content.md",
+        )
+
+    @task
+    def copywriting_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["copywriting"],
+            agent=self.copywriter(),
+        )
+
+    @task
+    def report_final_content_strategy(self) -> Task:
+        return Task(
+            config=self.tasks_config["report_final_content_strategy"],
+            agent=self.content_strategist(),
+            output_file="final-content-strategy.md",
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the Instagram crew"""
+        return Crew(
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=2,
+            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+        )
